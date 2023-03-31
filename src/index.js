ab6461f0ec9dc032343a8f6383c56cee4e601ffd
Reece Crook <reececrook16@gmail.com>
2023-03-30 19:33:43 -0600

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form")
+  form.addEventListener("submit", function() {
+    event.preventDefault()
+  });
+  form.addEventListener("submit", function(){
+    document.getElementById("tasks").textContent = form.querySelector("new-task-description").textContent
+  })
+
 });
